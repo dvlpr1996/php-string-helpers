455d98df918df08c5a8c15f18c1d0aeff31d378b
dvlpr1996 <nimajahanbakhshian@gmail.com>
2022-12-18 13:47:00 +0330

    improve display of errors

--- src/exceptions/LanguageFileIsNotArrayException.php
+++ src/exceptions/LanguageFileIsNotArrayException.php
@@ -6,8 +6,13 @@ use Exception;
 
 class LanguageFileIsNotArrayException extends Exception
 {
-    public function __construct(string $message, int $statusCode = 500)
+    public function __construct(string $message, int $statusCode = 500, $previous = null)
     {
-        parent::__construct($message, $statusCode);
+        parent::__construct($message, $statusCode, $previous);
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ' : ' . $this->message;
     }
 }
